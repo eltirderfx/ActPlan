5d3289b115c3d0768c8ade6ea8a7a3edb61deecd
elwandytirtana <eltirderfx@gmail.com>
2020-09-03 06:54:13 +0700

    update develop

--- lib/screens/home/Home.dart
+++ lib/screens/home/Home.dart
@@ -4,14 +4,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFEB532C),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          
-        ),
-      ),
+      body: Container(),
     );
   }
 }
